d917980e8f734fb3d0cac9fcddcb791b84878075
Serokles <timbo.alexander@gmail.com>
2021-02-25 22:04:21 +0000

    Removing host lookup

--- data.tf
+++ data.tf
@@ -2,9 +2,9 @@ data "vsphere_datacenter" "dc" {
   name       = "HomeLab"
 }
 
-data "vsphere_host" "host" {
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
+#data "vsphere_host" "host" {
+#  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+#}
 
 
 data "vsphere_host" "hosts" {
